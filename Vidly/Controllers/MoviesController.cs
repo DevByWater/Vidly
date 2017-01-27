17ce014c719f64fe526ad03a36b4e0a55ee381e8
DevByWater <egaraudy@gmail.com>
2017-01-26 18:54:41 -0700

    Created Customer Model, Controller, and View Created Movie Model, Controller, and View

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m=>m.Id==id);
+            if (movie == null)
+                return HttpNotFound();
+            return View(movie);
+        }
+           
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name="Die Hard"},
+                new Movie { Id = 2, Name="Zootopia" }
+            };
+        }
+
+    }
+}
