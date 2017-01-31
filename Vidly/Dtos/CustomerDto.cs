a294de840813ceaebb206529129b3f62d80f2821
DevByWater <egaraudy@gmail.com>
2017-01-31 16:41:29 -0700

    Built Customers Api

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public DateTime? DoB { get; set; }
+
+        public bool IsSuscribedToNewsLetter { get; set; }
+
+        [Required]
+       // [Min18Member]
+        public byte MembershipTypeId { get; set; }
+    }
+}
